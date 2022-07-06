acc226040457aaed2452afe369e19b9347c7a06d
Peerawas Archavanuntakun <peerawas.a@gmail.com>
2022-07-06 16:20:18 +1000

    fix lint running on old folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     node: true,
     jest: true
   },
-  ignorePatterns: [".eslintrc.js", "dist/*"],
+  ignorePatterns: [".eslintrc.js", "dist/*", "old/*"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
