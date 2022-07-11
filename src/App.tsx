0fd3b43c555216008a64ee79633b3685a38940e3
JasminFarhi <jasmin.farhi@dltx.io>
2022-07-11 10:03:11 +1000

    Styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,26 @@ import CountdownTimer from "./components/CountDownTimer";
 export const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative">
-        <img src={background} className="absolute top-0 left-0" />
-        <div className="z-10 relative">
-          <Navbar />
-          <div className="mt-20">
-            <div className="flex-col justify-center text-[#FFFFFF] text-center mt-10 text-5xl">
-              <div className="flex justify-center text-[#FFFFFF] mt-5 text-2xl mb-10">
-                Coming soon...
-              </div>
-              <div className="flex justify-center text-center">
-                <CountdownTimer />
+      <div className="min-width: 1920px; margin: 0 auto; max-height: 1080px;">
+        <div className="relative">
+          <img src={background} className="absolute top-0 left-0" />
+          <div className="z-10 relative">
+            <Navbar />
+            <div className="mt-20">
+              <div className="flex-col justify-center text-[#FFFFFF] text-center mt-10 text-5xl">
+                <div className="flex justify-center text-[#FFFFFF] mt-5 text-2xl mb-10">
+                  Coming soon...
+                </div>
+                <div className="flex justify-center text-center">
+                  <CountdownTimer />
+                </div>
               </div>
             </div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
           </div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
         </div>
       </div>
     </BrowserRouter>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import { Logo } from "../assets";
-import background from "../assets/background.png";
 
 const Navbar = () => {
   return (
     <>
       <div>
         <Logo width="493" height="430" />
-        <div className="">
+        <div className="min-w-960 md:my-auto">
           <div className="flex justify-end space-x-5 mr-5 text-[#FAEE6C] font-thin text-4xl font-roboto drop-shadow">
             <div className="">Admin</div> <div className="">Sign Up</div>
             <div className="">Documentation</div> <div className="">GitHub</div>
