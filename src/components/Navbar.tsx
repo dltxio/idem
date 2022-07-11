c2d1211a21a4eb18c6f8365b999df737e19af58a
JasminFarhi <jasmin.farhi@dltx.io>
2022-07-11 11:05:31 +1000

    Added styling and Linted file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,11 @@ import { Logo } from "../assets";
 const Navbar = () => {
   return (
     <>
-      <div>
-        <Logo width="493" height="430" />
-        <div className="min-w-960 md:my-auto">
-          <div className="flex justify-end space-x-5 mr-5 text-[#FAEE6C] font-thin text-4xl font-roboto drop-shadow">
-            <div className="">Admin</div> <div className="">Sign Up</div>
-            <div className="">Documentation</div> <div className="">GitHub</div>
-          </div>
+      <div className="flex flex-col">
+        <Logo width="35%" height="35%" />
+        <div className="flex justify-end space-x-5 mr-5 text-[#FAEE6C] font-thin text-4xl font-roboto drop-shadow">
+          <div className="">Admin</div> <div className="">Sign Up</div>
+          <div className="">Documentation</div> <div className="">GitHub</div>
         </div>
       </div>
     </>
