338dc8bba46f6704d794c353beadcb84e46a786b
JasminFarhi <jasmin.farhi@dltx.io>
2022-07-12 14:05:31 +1000

    Added first webpage with styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,8 @@ import { Logo } from "../assets";
 const Navbar = () => {
   return (
     <>
-      <div className="flex flex-col">
-        <Logo width="35%" height="35%" />
-        <div className="flex justify-end space-x-5 mr-5 text-[#FAEE6C] font-thin text-4xl font-roboto drop-shadow">
-          <div className="">Admin</div> <div className="">Sign Up</div>
-          <div className="">Documentation</div> <div className="">GitHub</div>
-        </div>
+      <div className="flex justify-center pt-10">
+        <Logo width="15%" height="15%" />
       </div>
     </>
   );
