5325059e46cfe5377ff8ea6216449dd6be27c091
JasminFarhi <jasmin.farhi@dltx.io>
2022-07-15 14:09:31 +1000

    Used form Lib

--- src/apis/Api.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-// import HTTPClient from "./HTTPClient";
-// export default class Api extends HTTPClient {
-//   public addUser = async (body: SubmitEvent) =>
-//     this.post<string>(`user/signup`, body);
-// }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,100 +1,83 @@
-import { useState } from "react";
 import axios from "axios";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
 
 const RedeemCodeForm = () => {
-  const [userDetails, setUserDetails] = useState<string>();
-
-  const users: {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-  }[] = [];
-  const addUser = (ev: SubmitEvent) => {
-    ev.preventDefault();
-    const user = {
-      id: Date.now(),
-      firstName: document.getElementById("firstName")!.innerText,
-      lastName: document.getElementById("lastName")!.innerText,
-      email: document.getElementById("email")!.innerText
-    };
-    users.push(user);
-    document.forms[0].reset();
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    axios
+      .post(`https://proxy.idem.com.au/registerTester`, { data })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        alert(
+          "We have submitted your request. Please check your email for a confirmation."
+        );
+      })
+      .catch((err) => {
+        alert("ERROR, please try again");
+      });
   };
-
-  document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("btn")!.addEventListener("submit", addUser);
-  });
-
-  axios.post(`"https://proxy.idem.com.au/"`, { users }).then((res) => {
-    console.log(res);
-    console.log(res.data);
-  });
 
   return (
     <>
       <div className="w-50 h-50 flex-col justify-center mt-10">
-        <form className="flex-col">
+        <div>
+          <label className="text-[#ffd000] text-5xl pt-50">Contact</label>
+        </div>
+        <div>
+          <label className="text-white text-xl">
+            Want to try IDEM? Join our test group today!
+          </label>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex-col">
           <div>
-            <label className="text-[#ffd000] text-5xl pt-50">Contact</label>
-          </div>
-          <div>
-            <label className="text-white text-xl">
-              Want to try IDEM? Join our test group today!
-            </label>
-          </div>
-          <div>
-            <label className="text-white text-xl">
+            <label htmlFor="firstName" className="text-white text-xl">
               First Name
-              <div>
-                <input
-                  className=" outline-[#ffd000] rounded-3xl text-white bg-black border-white border-2 ml-5 mt-2 px-2 py-2 text-sm w-80"
-                  type="text"
-                  id="firstName"
-                  placeholder="Write here..."
-                  onChange={(e) => setUserDetails(e.target.innerText)}
-                />
-              </div>
             </label>
+            <div>
+              <input
+                className=" outline-[#ffd000] rounded-3xl text-white bg-black border-white border-2 ml-5 mt-2 px-2 py-2 text-sm w-80"
+                placeholder="Write here..."
+                {...register("firstName")}
+              />
+            </div>
           </div>
+
           <div>
-            <label className="text-white text-xl">
+            <label htmlFor="lastName" className="text-white text-xl">
               Last Name
-              <div>
-                <input
-                  className="outline-[#ffd000] rounded-3xl text-white bg-black border-white mt-2 border-2 ml-5 px-2 py-2 text-sm w-80"
-                  type="text"
-                  id="lastName"
-                  placeholder="Write here..."
-                  onChange={(e) => setUserDetails(e.target.innerText)}
-                />
-              </div>
             </label>
+            <div>
+              <input
+                placeholder="Write here..."
+                className="outline-[#ffd000] rounded-3xl text-white bg-black border-white mt-2 border-2 ml-5 px-2 py-2 text-sm w-80"
+                {...register("lastName")}
+              />
+            </div>
           </div>
           <div>
-            <label className="text-white text-xl mr-8">
+            <label htmlFor="email" className="text-white text-xl mr-8">
               Email
-              <div>
-                <input
-                  className="outline-[#ffd000] rounded-3xl text-white bg-black border-white border-2 ml-5 mt-2 px-2 py-2 text-sm w-80"
-                  type="text"
-                  id="email"
-                  placeholder="Write here..."
-                  onChange={(e) => setUserDetails(e.target.innerText)}
-                />
-              </div>
             </label>
+            <div>
+              <input
+                placeholder="Write here..."
+                className="outline-[#ffd000] rounded-3xl text-white bg-black border-white border-2 ml-5 mt-2 px-2 py-2 text-sm w-80"
+                type="email"
+                {...register("email")}
+              />
+            </div>
           </div>
-          <div>
-            <button
-              className="bg-[#ffd000] text-black text-sm rounded-3xl px-5 py-2 mb-10 hover:bg-[#ab8d09]"
-              type="submit"
-              value="Submit"
-              disabled={!userDetails}
-            >
-              SEND
-            </button>
-          </div>
+          <input
+            type="submit"
+            className="bg-[#ffd000] text-black text-sm rounded-3xl px-5 py-2 mb-10 hover:bg-[#ab8d09]"
+          />
         </form>
       </div>
     </>
