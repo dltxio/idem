f19d3329076eeb33a40631628f128b11ccc64283
Alex Xiang <alex.xiang@dltx.io>
2022-07-20 13:12:01 +1000

    fix deployment issu

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
     react(),
-    reactRefresh(),
     svgrPlugin({
       svgrOptions: {
         icon: true
